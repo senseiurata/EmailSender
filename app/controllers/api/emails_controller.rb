59b89924f63ecf034deade82ee8514522248c5bd
Ken Urata <senseiurata@gmail.com>
2014-06-11 16:50:26 -0700

    set up custom route and EmailsController function

--- app/controllers/api/emails_controller.rb
+++ app/controllers/api/emails_controller.rb
@@ -1,5 +1,7 @@
 module Api
   class EmailsController < ApplicationController
-
+    def send_email
+      
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-
+    post "send_email", to: "emails#send_email"
   end
 end
