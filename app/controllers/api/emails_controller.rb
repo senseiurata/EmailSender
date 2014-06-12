8e7587c7f174b1f0868d7b53975e3b4f0805c5ac
Ken Urata <senseiurata@gmail.com>
2014-06-12 16:57:36 -0700

    add validation

--- app/controllers/api/emails_controller.rb
+++ app/controllers/api/emails_controller.rb
@@ -3,14 +3,34 @@ module Api
     protect_from_forgery except: [:send_email]
 
     def send_email
-      emailer = Emailer.send_email(email_params)
+      email_attr = email_params
+      
+      email_attr["to"] = parse_email(email_attr["to"])
 
-      emailer.deliver
+      #explicitly set to empty string. otherwise default subject is function name "Send email"
+      email_attr["subject"] ||= ""
 
-      head :ok
+      unless email_attr["to"].nil?
+        emailer = Emailer.send_email(email_attr)
+
+        emailer.deliver
+
+        head :ok
+      else
+        @errors ||= []
+        @errors.push("Invalid email!")
+
+        render json: { errors: @errors }, status: :unprocessable_entity
+      end
     end
 
     private
+
+    def parse_email(email)
+      matches = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i.match(email)
+
+      matches ? matches[0] : nil
+    end
 
     def email_params
       params.require(:email).permit(:to, :subject, :body)
--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -2,9 +2,9 @@ class Emailer < ActionMailer::Base
   default from: ENV["emailer_from"],
           reply_to: ENV["emailer_from"]
 
-  def send_email(email_params)
-    @body = email_params["body"]
+  def send_email(email_attr)
+    @body = email_attr["body"]
 
-    mail(to: email_params["to"], subject: email_params["subject"])
+    mail(to: email_attr["to"], subject: email_attr["subject"])
   end
 end
