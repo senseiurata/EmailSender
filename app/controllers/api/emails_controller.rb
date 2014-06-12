861e2bf5395e259b410cac52c6d539b9fcb95e48
Ken Urata <senseiurata@gmail.com>
2014-06-11 21:26:46 -0700

    remove protect from forgery on send_mail for EmailsController

--- app/controllers/api/emails_controller.rb
+++ app/controllers/api/emails_controller.rb
@@ -1,7 +1,13 @@
 module Api
   class EmailsController < ApplicationController
+    protect_from_forgery except: [:send_email]
+
     def send_email
-      
+      emailer = Emailer.send_email
+
+      emailer.deliver
+
+      render json: {}
     end
   end
 end
