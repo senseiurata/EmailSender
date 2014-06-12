d845719e0058f80f85ac2b5d58a40768786854d0
Ken Urata <senseiurata@gmail.com>
2014-06-12 14:04:24 -0700

    revert smtp settings back to gmail

--- config/environment.rb
+++ config/environment.rb
@@ -2,24 +2,14 @@
 require File.expand_path('../application', __FILE__)
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV["sendgrid_user_name"],
-  :password => ENV["sendgrid_password"],
+  :user_name => ENV["gmail_user_name"],
+  :password => ENV["gmail_password"],
   :domain => 'example.com',
-  :address => 'smtp.sendgrid.net',
+  :address => 'smtp.gmail.com',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
-
-# ActionMailer::Base.smtp_settings = {
-#   :user_name => ENV["yahoo_user_name"],
-#   :password => ENV["yahoo_password"],
-#   :domain => 'example.com',
-#   :address => 'smtp.mail.yahoo.com',
-#   :port => 587,
-#   :authentication => :plain,
-#   :enable_starttls_auto => true
-# }
 
 # Initialize the Rails application.
 Rails.application.initialize!
